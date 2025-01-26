1af3ea32dc35ec7d97fda5ee6b76c2a1dcee2ff3
jcenteno-ucm <jorgcente@ucm.es>
2025-01-26 17:18:50 +0100

    Added Kafka Connect Content

--- 1.environment/install-connect-plugins.sh
+++ 1.environment/install-connect-plugins.sh
@@ -1,12 +1,22 @@
 #/bin/bash
 
-docker-compose exec connect confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:latest
+docker compose exec connect confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:latest
 
-docker-compose exec connect confluent-hub install --no-prompt confluentinc/kafka-connect-jdbc:latest
+docker compose exec connect confluent-hub install --no-prompt confluentinc/kafka-connect-jdbc:latest
 
 docker cp ./mysql/mysql-connector-java-5.1.45.jar connect:/usr/share/confluent-hub-components/confluentinc-kafka-connect-jdbc/lib/mysql-connector-java-5.1.45.jar
 
-docker-compose restart connect
+#docker compose exec connect confluent-hub install --no-prompt mongodb/kafka-connect-mongodb:latest
+#docker compose exec connect confluent-hub install --no-prompt neo4j/kafka-connect-neo4j:latest
+docker compose exec connect confluent-hub install --no-prompt confluentinc/kafka-connect-elasticsearch:latest
+#docker compose exec connect confluent-hub install --no-prompt confluentinc/kafka-connect-hdfs:latest
+#docker compose exec connect confluent-hub install --no-prompt confluentinc/kafka-connect-cassandra:latest
+#docker compose exec connect confluent-hub install --no-prompt confluentinc/kafka-connect-azure-blob-storage:latest
+#docker compose exec connect confluent-hub install --no-prompt confluentinc/kafka-connect-jms:latest
+#docker compose exec connect confluent-hub install --no-prompt confluentinc/kafka-connect-file:latest
+#docker compose exec connect confluent-hub install --no-prompt confluentinc/kafka-connect-ftp:latest
+
+docker compose restart connect
 
 echo "Esperando 30s a que connect reinicie"
 
